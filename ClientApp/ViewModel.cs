32fff3071d4cc8f2de0b996a3eeb4456e702f6c4
Роман Перехрестюк <Pere_qw49@student.itstep.org>
2023-05-08 17:08:26 +0300

    Added class ViewModel

--- ClientApp/ViewModel.cs
+++ ClientApp/ViewModel.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClientApp
 {
-    internal class ViewModel
+    [AddINotifyPropertyChangedInterface]
+    class ViewModel
     {
+        public string SongSize { get; set; }
+        public ViewModel()
+        {
+            songs = new ObservableCollection<Song>();
+            SongSize = $"Songs Count: {Songs.Count()}";
+        }
+        ObservableCollection<Song> songs;
+        public IEnumerable<Song> Songs => songs;
+        public void AddSongs(Song song)
+        {
+            songs.Add(song);
+            SongSize = $"Songs Count: {Songs.Count()}";
+        }
+        public void Clear()
+        {
+            songs.Clear();
+        }
     }
 }
